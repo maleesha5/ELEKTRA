464b898e94f83748dcd624f4b337cb9e4b8fe42e
maleesha5 <maleesha55@gmail.com>
2017-03-13 00:55:59 +0530

    addDevice first steps

--- app/src/main/java/com/example/kokil/elektra/AddDevice.java
+++ app/src/main/java/com/example/kokil/elektra/AddDevice.java
@@ -1,6 +1,7 @@
 package com.example.kokil.elektra;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -23,6 +24,13 @@ public class AddDevice extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.wifi, container, false);
+
+        {
+           // Intent intent = new Intent(container,Wifidemo.class);
+           // startActivity(intent);
+        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_device, container, false);
     }
