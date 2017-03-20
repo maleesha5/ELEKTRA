d658fae96782130b6b8f04b0ea1ade3cba003c0d
kokilayaa <kokilapc@gmail.com>
2017-03-21 00:30:53 +0530

    Wifi On/Off

--- app/src/main/java/com/example/kokil/elektra/AddDevice.java
+++ app/src/main/java/com/example/kokil/elektra/AddDevice.java
@@ -1,11 +1,16 @@
 package com.example.kokil.elektra;
 
 
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import static android.content.Context.WIFI_SERVICE;
 
 
 /**
@@ -13,6 +18,9 @@ import android.view.ViewGroup;
  */
 //This is to check gitHub
 public class AddDevice extends Fragment {
+
+    Switch wifiSwitch;
+    WifiManager wifiManager;
 
 
     public AddDevice() {
@@ -24,7 +32,24 @@ public class AddDevice extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_device, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_add_device, container, false);
+        wifiSwitch = (Switch) rootView.findViewById(R.id.wifiSwitch);
+        wifiManager = (WifiManager) getActivity().getSystemService(WIFI_SERVICE);
+        wifiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // To on Wifi
+                if (isChecked && !wifiManager.isWifiEnabled()) {
+                    wifiManager.setWifiEnabled(true);
+                }
+                //To off Wifi
+                else if (!isChecked && wifiManager.isWifiEnabled()){
+                    wifiManager.setWifiEnabled(false);
+                }
+            }
+        });
+
+
+        return rootView;
     }
 
 }
