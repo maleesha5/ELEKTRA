c89ad5b57e0167d5010a40a02c46d6d5dd815e4e
sankalpa <sankalpa656@gmail.com>
2017-04-21 20:54:27 +0530

    shedule java change
    xml need to be change

--- app/src/main/java/com/example/kokil/elektra/Schedule.java
+++ app/src/main/java/com/example/kokil/elektra/Schedule.java
@@ -30,75 +30,218 @@ public class Schedule extends Fragment {
     AlarmManager alarmManager;
     long time;
 
-    public Schedule() {
+    Button b_pick,cancel;
+    TextView result;
+    TextView result2;
+    TextView result3;
+    TextView result4;
+    CountDownTimer countDownTimer;
 
-    }
+    int day,month,year,hour,minute,second;                      //current date variables
+    int dayFinal,monthFinal,yearFinal,hourFinal,minuteFinal;    //user selected date variables
+    int dayPre,monthPre,yearPre,hourPre,minutePre,secondPre;     // current date in miliseconds variables
+    int hourPas,minutePas,monthPas,dayPas;                      //user selected date in miliseconds
+    int totalPre,totalPas,difference;                           //date difference and total variables
 
-    @Nullable
+
+
+    private View.OnClickListener btnOnClickListener= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {  // checking button is clicked
+            switch(v.getId()){
+                case R.id.b_pick:
+                    start();             //start button
+                    break;
+                case R.id.cancel:
+                    cancel();              //cancel button
+                    break;
+
+            }
+
+        }
+    };
+
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_schedule, container, false);
-        ToggleButton toggle = (ToggleButton) rootView.findViewById(R.id.toggleButton);
-        alarmTimePicker = (TimePicker) rootView.findViewById(R.id.timePicker);
-        alarmManager = (AlarmManager) getActivity().getSystemService(ALARM_SERVICE);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton toggle, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    Toast.makeText(getActivity(), "ALARM ON", Toast.LENGTH_SHORT).show();
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
-                    calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
-                    Intent intent = new Intent(getActivity(), AlarmReceiver.class);
-                    pendingIntent = PendingIntent.getBroadcast(getActivity(), 0, intent, 0);
+        b_pick=(Button) findViewById(R.id.b_pick);
+        b_pick.setOnClickListener(btnOnClickListener);
+        cancel=(Button) findViewById(R.id.cancel);
+        cancel.setOnClickListener(btnOnClickListener);
+        result=(TextView) findViewById(R.id.result);
+        result2=(TextView) findViewById(R.id.result2);
+        result3=(TextView) findViewById(R.id.result3);
+        result4=(TextView) findViewById(R.id.result4);
 
-                    time=(calendar.getTimeInMillis()-(calendar.getTimeInMillis()%60000));
-                    if(System.currentTimeMillis()>time)
-                    {
-                        if (calendar.AM_PM == 0)
-                            time = time + (1000*60*60*12);
-                        else
-                            time = time + (1000*60*60*24);
-                    }
-                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
-                } else {
-                    // The toggle is disabled
-                    alarmManager.cancel(pendingIntent);
-                    Toast.makeText(getActivity(), "ALARM OFF", Toast.LENGTH_SHORT).show();
-                }
+      /*  b_pick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                start();
+
+
+
             }
         });
-        return rootView;
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cancel();
+
+
+
+            }
+        });*/
     }
 
-    /*public void OnToggleClicked(View view)
-    {
-        long time;
-        if (((ToggleButton) view).isChecked())
-        {
-            Toast.makeText(getActivity(), "ALARM ON", Toast.LENGTH_SHORT).show();
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
-            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
-            Intent intent = new Intent(getActivity(), AlarmReceiver.class);
-            pendingIntent = PendingIntent.getBroadcast(getActivity(), 0, intent, 0);
 
-            time=(calendar.getTimeInMillis()-(calendar.getTimeInMillis()%60000));
-            if(System.currentTimeMillis()>time)
-            {
-                if (calendar.AM_PM == 0)
-                    time = time + (1000*60*60*12);
-                else
-                    time = time + (1000*60*60*24);
+    private void start(){   // setting date in date picker to current date
+
+        Calendar c= Calendar.getInstance();
+        year=c.get(Calendar.YEAR);
+        month=c.get(Calendar.MONTH)+1;
+        day=c.get(Calendar.DAY_OF_MONTH);
+        hour=c.get(Calendar.HOUR_OF_DAY);
+        minute=c.get(Calendar.MINUTE);
+        second=c.get(Calendar.SECOND);
+
+
+
+
+        result2.setText(year+" "+month+" "+day+ " "+hour+ " "+minute+ " "
+                +second);
+
+        DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this, MainActivity.this, year, month, day);
+        datePickerDialog.show();
+
+    }
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {  //user selected month day
+
+        // yearFinal=year;
+        monthFinal=monthOfYear;
+        dayFinal=dayOfMonth;
+
+        //Calendar c=Calendar.getInstance();
+
+        TimePickerDialog timePickerDialog=new TimePickerDialog(MainActivity.this, MainActivity.this,hour, minute,
+                DateFormat.is24HourFormat(this));
+        timePickerDialog.show();
+
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {  // user selected hour and minute
+        hourFinal=hourOfDay;
+        minuteFinal=minute;
+
+
+
+
+
+
+
+
+        result.setText("Year :"+yearFinal+"\nMonth :"+monthFinal+"\nDay :"+day+"\nHour :"+hourFinal+"\nMinute :"+minuteFinal);
+        //result2.setText(yearFinal);
+
+        current();
+    }
+
+    private void current() {
+
+        Calendar c= Calendar.getInstance();
+        year=c.get(Calendar.YEAR);
+        month=c.get(Calendar.MONTH)+1;
+        day=c.get(Calendar.DAY_OF_MONTH);
+        hour=c.get(Calendar.HOUR_OF_DAY);
+        minute=c.get(Calendar.MINUTE);
+        second=c.get(Calendar.SECOND);
+
+        //default values to convert month,day,hour,minute and seconds into miliseconds
+
+        final int month_def=262800288*10;
+        final int day_def=86400000;
+        final int hour_def=3600000;
+        final int minute_def=60000;
+        final int sec_def=1000;
+
+
+
+
+        result4.setText(year+" "+month+" "+day+ " "+hour+ " "+minute+ " "
+                +second);
+
+        monthPre=month*month_def;       //current month in miliseconds
+        dayPre=day*day_def;             //current day in miliseconds
+        hourPre=hour*hour_def;          //current hour in miliseconds
+        minutePre=minute*minute_def;    //current minute in miliseconds
+        secondPre=second*sec_def;       //current second in miliseconds
+
+        totalPre=hourPre+minutePre+secondPre+monthPre+dayPre;   //current date in miliseconds
+
+        monthPas=monthFinal*month_def;              //user entered month in miliseconds
+        dayPas=dayFinal*day_def;                    //user entered day in miliseconds
+        hourPas=hourFinal*hour_def;                 //user entered hour in miliseconds
+        minutePas=minuteFinal*minute_def;           //user entered minute in miliseconds
+
+        totalPas=hourPas+minutePas+monthPas+dayPas;  // user entered date in miliseconds
+
+        difference=(totalPas-totalPre);         //date difference in miliseconds
+        result3.setText("the dif ="+difference);
+
+
+        if(difference>0) {
+            count();
+        }else{
+            result3.setText("Incorrect Input,,");
+        }
+
+
+    }
+
+    private void count() {
+        countDownTimer : new CountDownTimer(difference,1000) {      // setting countdown time as date difference in miliseconds
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                result3.setText("count"+millisUntilFinished/1000);
+
             }
-            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
-        }
-        else
-        {
-            alarmManager.cancel(pendingIntent);
-            Toast.makeText(getActivity(), "ALARM OFF", Toast.LENGTH_SHORT).show();
-        }
-    }*/
+
+            @Override
+            public void onFinish() {
+                result3.setText("finished"); // on finish set text to finished
+                //  Vibrator v=(Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+                // v.vibrate(500);
+                // countDownTimer.cancel();
+
+            }
+        }.start();  // starting the countdown timer
+    }
+
+
+    // cancel countdown
+    public void cancel(){
+        result2.setText("canceled");
+        countDownTimer.cancel();
+/*if(countDownTimer!=null) {
+    countDownTimer.cancel();
+    result3.setText("canceled");
+    countDownTimer = null;
+}*/
+
+
+
+
+
+    }
 }
