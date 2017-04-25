bdb74935558b53d13dd9763993caf5763daae56d
HemalHerath <hemal.2015327@iit.ac.lk>
2017-04-25 22:09:40 +0530

    Test
    Testing

--- app/src/main/java/com/example/kokil/elektra/WeatherStatus.java
+++ app/src/main/java/com/example/kokil/elektra/WeatherStatus.java
@@ -33,12 +33,14 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 import static com.google.android.gms.internal.zzs.TAG;
 
 /**
- * Created by Hemal on 4/7/2017.
+ * Created by Dell on 4/7/2017.
  */
 
 class WeatherStatus extends AsyncTask<Void, Void, Void> {
+
     @Override
     protected void onPreExecute() {
+
         super.onPreExecute();
         //  Toast.makeText(MainActivity.this,"Json Data is
         //          downloading",Toast.LENGTH_LONG).show();
@@ -47,6 +49,7 @@ class WeatherStatus extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected Void doInBackground(Void... arg0) {
+
         HttpHandler sh = new HttpHandler();
         // Making a request to url and getting response
         String url = "http://api.openweathermap.org/data/2.5/weather?q=Kurunegala&appid=6b305548a0cfd94c7fa2bcbc99037db8";
@@ -54,13 +57,17 @@ class WeatherStatus extends AsyncTask<Void, Void, Void> {
 
         Log.e(TAG, "Response from url: " + jsonStr);
         if (jsonStr != null) {
+
             try {
+
                 JSONObject jsonObj = new JSONObject(jsonStr);
 
                 // Getting JSON Array node
+
                 JSONArray weather = jsonObj.getJSONArray("weather");
 
                 // looping through All Contacts
+
                 for (int i = 0; i < weather.length(); i++) {
                     JSONObject c = weather.getJSONObject(i);
                     String id = c.getString("id");
@@ -124,6 +131,7 @@ class WeatherStatus extends AsyncTask<Void, Void, Void> {
             }
 
         } else {
+            
             Log.e(TAG, "Couldn't get json from server.");
             //  runOnUiThread(new Runnable() {
                /*     @Override
@@ -140,6 +148,7 @@ class WeatherStatus extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected void onPostExecute(Void result) {
+
          /*   super.onPostExecute(result);
             ListAdapter adapter = new SimpleAdapter(MainActivity.this, contactList,
                     R.layout.list_item, new String[]{ "email","mobile"},
